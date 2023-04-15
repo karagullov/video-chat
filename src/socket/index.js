111f851c49d1fd424423d3a577bcf479594d0f07
erlannn777 <erlank123456789@gmail.com>
2023-04-16 03:01:26 +0600

    sockets settings updated

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,8 +2,8 @@ import { io } from "socket.io-client";
 
 const options = {
   "force new connection": true,
-  reconnectinAttempts: "Infinity",
-  timeout: 10000,
+  reconnectionAttempts: "Infinity", // avoid having user reconnect manually in order to prevent dead clients after a server restart
+  timeout: 10000, // before connect_error and connect_timeout are emitted.
   transports: ["websocket"],
 };
 
